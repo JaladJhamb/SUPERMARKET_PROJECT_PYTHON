20e98511c08216cb5d44e9e30ae0c73a915c0adb
Jalad Jhamb <jaladjhamb@gmail.com>
2021-08-05 02:37:55 +0530

    adding files for Supermarket Project through Python

---
+++ README.md
@@ -0,0 +1,2 @@
+#In this repository a normal data has been taken of a supermart and with the help of file handling,python a project has been made on python coding.
+ 
---
+++ dictionary.py
@@ -0,0 +1,63 @@
+stationary={
+            'pen':{'brand':{'link':{'price':[5,10],'stock':[200,100]},
+                            'fine':{'price':[5,10],'stock':[250,150]}
+                           }
+                  },
+            'register':{'brand':{'classmate':{'price':[50,80],'stock':[100,50]},
+                            'icon':{'price':[50,60],'stock':[200,80]}
+                           }
+                      },
+            'books':{'class VI':{'price':500,'stock':20},
+                     'class VII':{'price':600,'stock':20},
+                     'class VIII':{'price':700,'stock':20},
+                     'class IX':{'price':800,'stock':20},
+                     'class X':{'price':900,'stock':20},
+                    }
+}
+
+electronics={
+            'mobile':{'brand':{'one plus':{'model':['7 pro','8 pro','7'],'price':[40000,46000,35000],'stock':[20,20,10]},
+                              'iphone':{'model':['6','10','11'],'price':[20000,56000,100000],'stock':[20,20,10]},
+                              'MI':{'model':['10 pro','8 pro','8A','7 pro','6A'],'price':[40000,15000,9000,10000,8000],'stock':[20,50,50,30,100]}
+                             }
+                  },
+            'laptop':{'brand':{'hp':{'model':['HP Pavilion Gaming 15','HP Omen 15','HP Spectre x360 (2019)'],'price':[55000,95000,150000],'stock':[5,5,5]},
+                              'samsung':{'model':['Samsung ChromeBook XE50','Samsung ChromeBook 3 XE501C13-K01US','Samsung Spin 7 NP730QAA'],'price':[25000,46000,90000],'stock':[20,20,10]},
+                              'dell':{'model':['Dell Inspiron Core','Dell Inspiron Touchscreen 15 5548 Notebook','Dell Vostro 3458'],'price':[25000,75000,29000],'stock':[15,10,20]}
+                             }
+                  }
+}
+
+
+
+cosmatic={
+            'bodylotion':{'brand':{'joy':{'price':[50,100],'stock':[100,50]},
+                                   'vaseline':{'price':[120,200],'stock':[120,100]}
+                           }
+                  },
+            'soap':{'brand':{'lifeboy':{'price':[10,20],'stock':[100,50]},
+                            'dettol':{'price':[20,40],'stock':[200,80]},
+                            'godrej no.1 ':{'price':[15,25],'stock':[200,80]},
+                           }
+                },
+            'cream':{'brand':{'boroplus':{'price':[20,40],'stock':[100,50]},
+                            'fair Handsome':{'price':[20,40],'stock':[200,80]},
+                            'meglow':{'price':[50,100],'stock':[200,80]},
+                           }
+                   },
+            'perfume':{'brand':{'denver':{'price':[100,200],'stock':[100,50]},
+                            'park avenue':{'price':[120,180],'stock':[200,80]},
+                            'fogg':{'price':[150,195],'stock':[200,80]},
+                           }
+                   }
+}
+
+
+
+chocolate={
+            'dairymilk':{'price':[20,40,60,80,100],'stock':[100,90,80,70,60]},
+            'munch':{'price':[5,10,20,40,60],'stock':[100,90,80,70,60]},
+            'kit-kat':{'price':[5,10,20,40,60],'stock':[100,90,80,70,60]},
+            'five star':{'price':[5,10,20,40],'stock':[100,80,70,60]},
+            'perk':{'price':[5,10,20,40],'stock':[100,90,80,70]}
+}
